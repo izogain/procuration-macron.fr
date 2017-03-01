c2126b0f195e297c71ed5610197aab38beaff0db
Titouan Galopin <galopintitouan@gmail.com>
2017-03-01 21:22:12 +0100

    Add CircleCI auto deployment

--- web/app.php
+++ web/app.php
@@ -2,6 +2,16 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+if (!empty($_SERVER['BASIC_AUTH_USER']) && !empty($_SERVER['BASIC_AUTH_PASSWORD']) && !empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+    if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] !== $_SERVER['BASIC_AUTH_USER'] || $_SERVER['PHP_AUTH_PW'] !== $_SERVER['BASIC_AUTH_PASSWORD']) {
+        header('HTTP/1.0 401 Unauthorized');
+        header('WWW-Authenticate: Basic realm="Password required"');
+
+        echo 'Unauthorized';
+        exit;
+    }
+}
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
