415f02395c76baaa43a5787d33c45a1c26ee2af5
Xavier Coureau <xavier@pandawan-technology.com>
2017-03-07 09:53:54 +0100

    Fixed display

--- src/AppBundle/Twig/UiExtension.php
+++ src/AppBundle/Twig/UiExtension.php
@@ -2,17 +2,42 @@
 
 namespace AppBundle\Twig;
 
+use AppBundle\Mediator\UserMediator;
+
 class UiExtension extends \Twig_Extension
 {
     const PAGINATION_SELECTED_CLASS = 'form-step--is-active';
 
     /**
-     * @return array
+     * @var UserMediator
+     */
+    protected $userMediator;
+
+    /**
+     * @param UserMediator $userMediator
+     */
+    public function __construct(UserMediator $userMediator)
+    {
+        $this->userMediator = $userMediator;
+    }
+
+    /**
+     * @inheritdoc
      */
     public function getFunctions()
     {
         return [
             new \Twig_SimpleFunction('pagination_class', [$this, 'isPaginationSelected'], ['needs_context' => true]),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('gender', [$this->userMediator, 'getCivility']),
         ];
     }
 
